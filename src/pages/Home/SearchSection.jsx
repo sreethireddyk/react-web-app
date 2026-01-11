1833e10c4bf0664d6d8697ecfeb5df25bb13e44f
sreethireddyk <sreethireddy04@gmail.com>
2026-01-12 00:03:12 +0530

    Track Home pages folder

---
+++ src/pages/Home/SearchSection.jsx
@@ -0,0 +1,10 @@
+const SearchSection = () => {
+  return (
+    <div>
+      <input placeholder="Search location" />
+      <input placeholder="Price range" />
+    </div>
+  );
+};
+
+export default SearchSection;
