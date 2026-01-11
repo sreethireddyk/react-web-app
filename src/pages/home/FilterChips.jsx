f743416d4f9d83d35ac9850db4dd46835ec61c2c
sreethireddyk <sreethireddy04@gmail.com>
2026-01-11 22:51:05 +0530

    Add search, filter chips and footer

---
+++ src/pages/home/FilterChips.jsx
@@ -0,0 +1,12 @@
+const FilterChips = () => {
+  return (
+    <div>
+      <button>Buy</button>
+      <button>Rent</button>
+      <button>Commercial</button>
+      <button>Residential</button>
+    </div>
+  );
+};
+
+export default FilterChips;
