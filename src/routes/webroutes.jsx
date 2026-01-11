905e6808f4da6157927c7514e221c26c9ef6ba6a
sreethireddyk <sreethireddy04@gmail.com>
2026-01-11 23:54:01 +0530

    Normalize Home folder casing

--- src/routes/webroutes.jsx
+++ src/routes/webroutes.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/Home/HomePage";
 
 const WebRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<div>Home Page</div>} />
+          <Route index element={<HomePage />} />
           <Route path="properties" element={<div>Property Page</div>} />
         </Route>
       </Routes>
